c4b619246fe23a8e8faa4f88aba7d608cfafd72d
utkarshbhardwaj22 <utkarsh2000bhard@gmail.com>
2021-02-23 21:56:43 +0530

    Number systems

---
+++ venv/Practic2.py
@@ -0,0 +1,10 @@
+"""
+Number Systems
+"""
+
+Number=8
+
+print("Number in Decimal:",Number)
+print("Number in Decimal:",bin(Number))
+print("Number in Octal:",oct(Number))
+print("Number in Hexal:",hex(Number))
--- venv/Practice.py
+++ venv/Practice.py
@@ -2,4 +2,11 @@ profit=23.45
 profit2= 76.45
 
 total=profit+profit2
-print("total",total)
+("The total is {}. The type is {}. The id is {}".format(total,type(total),id(total)))
+
+total2=total
+
+del total
+#print(total) # total and corresponding data will be deleted
+
+print(total2)
